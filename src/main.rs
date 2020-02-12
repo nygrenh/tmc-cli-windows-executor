fd1ec20435bed4d6dff7fbba025877f9ec4c123a
Redande <ronttiantti@gmail.com>
2020-02-12 14:55:07 +0200

    Support umlauts

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::process::Child;
 use std::process::Command;
+use std::ffi::OsString;
 
 fn main() {
     let current_executable: PathBuf =
@@ -12,9 +13,9 @@ fn main() {
         .as_path()
         .parent()
         .expect("Error: could not find the tmc-cli install location.");
-    let jar_path: PathBuf = install_folder.join("tmc-cli.jar");
+    let jar_path_string: PathBuf = install_folder.join("tmc-cli.jar");
+    let jar_path: OsString = jar_path_string.into_os_string();
     let cmd_args: Vec<String> = env::args().into_iter().skip(1).collect();
-    let command_string = format!("java -jar {:?}", jar_path);
 
     let command;
     let first_arg;
@@ -29,7 +30,9 @@ fn main() {
 
     let mut handle: Child = Command::new(command)
         .arg(first_arg)
-        .arg(command_string.as_str())
+        .arg("java")
+        .arg("-jar")
+        .arg(jar_path)
         .args(cmd_args)
         .spawn()
         .expect("Failed to execute tmc-cli");
